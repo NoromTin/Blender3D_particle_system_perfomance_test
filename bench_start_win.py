b701bb7f3eb987615d8308a6951d180f6c098ae8
NoromTin <110286926+NoromTin@users.noreply.github.com>
2024-01-30 22:25:06 +0300

    first load

---
+++ bench_start_win.py
@@ -0,0 +1,153 @@
+
+from time import time
+
+from multiprocessing.connection import Listener, Client
+
+IPC_base_port = 6000
+
+IPC_addr_worker_ready   = ('localhost', IPC_base_port)
+IPC_addr_worker_result  = ('localhost', IPC_base_port + 1)
+
+if  __name__ != '__main__':
+    import os
+    import subprocess
+    blender_path = 'C:\\Program Files\\Blender Foundation\\Blender 3.5\\blender'
+
+if __name__ == '__main__':
+
+    from os import cpu_count
+    import os
+    import sys
+    
+    from time import sleep
+    from multiprocessing.pool import Pool
+    
+    ############################
+    ### config start###
+    
+    # number of worker - only for 'mp' test
+    mp_min      = 1
+    # mp_max = 'auto' # 'auto' - Automatic os detect, incl hyper-threading
+    mp_max      = 1    
+    mp_factor   = 1    # multiplier for overcore bench, for example 2 mean 24 threads for 12 logical cores
+    
+    # threads per worker - only for 'thread' test
+    tn_min      = 1         # recommended for mp test, but for 
+    tn_max      = 1    # 'auto' - Automatic os detect, incl hyper-threading
+    tn_factor   = 1         # multiplier for overthread bench
+    
+    os_type = 'windows' # i known it is dirty, need some solid refactoring
+    
+    # bench list . Bench type process are separate
+    # 'mp' - for workers(num of blender instances, every with 1 core, 
+    # 'thread' - one instance of blender, but starts with different num of cpu. 
+    bench_type_list = [
+                'mp',
+                'thread'
+                ]
+    # test type. for every - separate scene python file
+    bench_name_list = [
+                'movement', 
+                #'collision'
+                ]
+
+    out_to_console  = False
+    out_to_csv      = True
+
+    is_gui_debug    = False # True - running with gui, false - without
+
+    ### config end
+    ############################
+
+def start_worker(*args):
+
+    # os_type     = args[0]
+    bench_name  = args[1]
+    # bench_type  = args[2]
+    t_num       = args[3]
+    p_num       = args[4]
+    gui_arg     = args[5]
+    
+    # name of start script here
+    blender_args = gui_arg + ' -t ' + str(t_num) + ' -P "./scene_' + bench_name + '.py"' + ' -- -pn ' + str(p_num) 
+    cmd = '\"\"' + blender_path +'\" ' + blender_args +'\"'
+    # print(cmd)
+    os.system(cmd)
+
+    return None
+
+
+if __name__ == '__main__':
+
+    ### init params ###
+    
+    if mp_max == 'auto':
+        mp_max = cpu_count() * mp_factor
+
+    if tn_max == 'auto':
+        tn_max = cpu_count() * tn_factor
+
+    if not is_gui_debug:
+        gui_arg = '-b'
+    else:
+        gui_arg = ''
+        
+    i_bench = 1
+    
+    pool = Pool(processes=mp_max)
+    
+    bench_num = len(bench_name_list) *  ( (mp_max - mp_min + 1 if 'mp' in bench_type_list else 1) + (tn_max - tn_min + 1 if 'thread' in bench_type_list else 1) )
+    
+    def start_pool(args_list):
+    
+        global i_bench
+        print(f'bench {i_bench} of  {bench_num}')
+        
+        IPC_READY_recv       = Listener(IPC_addr_worker_ready)
+        
+        r = pool.starmap_async(start_worker, args_list)
+        
+        # IPC READY msg from workers
+        for i in range (len(args_list)):
+            msg = IPC_READY_recv.accept()
+        sleep(3)
+        
+        # IPC START message to workers
+        IPC_START_sender_arr = [Client(('localhost', IPC_base_port + 2 + i)) for i in range(1, len(args_list) + 1)]
+        
+        for IPC_START_sender in IPC_START_sender_arr:
+            IPC_START_sender.send('')
+        
+        # IPC RESULT get from workers
+        IPC_RESULT_recv = Listener(IPC_addr_worker_result)
+        calc_result = []
+        for i in range (len(args_list)):
+            calc_result.append(IPC_RESULT_recv.accept().recv())
+
+        r.wait()
+        return calc_result
+        
+    ### start bench
+    result = []
+    
+    for bench_name in bench_name_list:
+        for bench_type in bench_type_list:
+     
+            if bench_type == 'mp':
+                for mp_num in range(mp_min, mp_max + 1):
+                    args_list = [ (os_type, bench_name,bench_type, 1 ,mp_n, gui_arg) for mp_n in range(mp_min, mp_num + 1)]
+                    result.append( (args_list, start_pool(args_list) ) )
+                    i_bench +=1
+                    
+            elif bench_type == 'thread':
+                for tn_num in range(tn_min, tn_max + 1):
+                    args_list = [ (os_type, bench_name, bench_type, tn_num , 1, gui_arg) ]
+                    result.append( (args_list, start_pool(args_list) ) )
+                    i_bench +=1
+                    
+    # print('result', result)
+    
+    ### analisys
+    result_analisys =[]
+    for rec in result:
+        print(rec)
---
+++ scene_collision.py
@@ -0,0 +1,106 @@
+import bpy
+bpy.ops.wm.quit_blender()
+
+import sys
+from time import time
+
+import argparse
+parser = argparse.ArgumentParser(description='process some integers.')
+parser.add_argument('-pn', '--process_num', default='1')
+args = parser.parse_args(sys.argv[sys.argv.index("--")+1:])
+process_num = int(args.process_num)
+
+from multiprocessing.connection import Listener, Client
+
+IPC_READY_sender = Client(('localhost', 6000))
+
+import bpy
+import blend_render_info
+
+
+# scene settings, better dont change for shared benchmark
+frame_part_born     = 0
+subdiv              = 7
+scene_frame_end           = 1000
+part_velocity       = 1.0
+
+ps_timestep = 1/1000
+
+srcRadius = 1.0
+src_location = [0.0,0.0,0.0]
+src_rotation = [0.0,0.0,0.0]
+src_scale    = [1.0,1.0,1.0]
+
+### scene
+scene = bpy.context.scene
+for obj in scene.objects:
+    bpy.data.objects.remove(obj, do_unlink=True)
+bpy.context.scene.frame_end = scene_frame_end
+bpy.types.Scene(bpy.context.scene).use_gravity = False
+bpy.types.Scene(bpy.context.scene).gravity = (0, 0, 0)
+
+### source mesh
+bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=subdiv,radius=srcRadius, calc_uvs=False, enter_editmode=False
+                                        , align='WORLD', location=src_location, rotation=src_rotation, scale=src_scale)
+obj = bpy.context.active_object
+obj.name    =   'src_obj'
+obj.show_instancer_for_render = False
+obj.show_instancer_for_viewport = False
+src_vertex_num = len(bpy.data.objects[obj.name].data.vertices)
+
+### particle system
+obj.modifiers.new("part", type='PARTICLE_SYSTEM')
+ps = obj.particle_systems[0]
+settings = ps.settings
+settings.emit_from          = 'VERT'
+settings.use_emit_random    = False
+settings.physics_type       = 'NEWTON'
+settings.integrator         = 'RK4'
+settings.render_type        = 'OBJECT'
+settings.particle_size      = 0.001
+settings.display_method     = 'CROSS'
+settings.subframes = 0
+settings.timestep = ps_timestep
+settings.use_adaptive_subframes = True
+settings.normal_factor = part_velocity
+
+
+settings.display_percentage = 100
+settings.display_size       = 0.01
+settings.display_color      = 'NONE'
+settings.count              = src_vertex_num
+settings.lifetime           = scene_frame_end + 1
+settings.frame_start        = frame_part_born
+settings.frame_end          = frame_part_born
+
+# camera only for debug
+camera_data = bpy.data.cameras.new(name='Camera')
+camera_object = bpy.data.objects.new('Camera', camera_data)
+bpy.context.scene.collection.objects.link(camera_object)
+camera_object.location = [0,0,12]
+for area in bpy.context.screen.areas:
+    if area.type == 'VIEW_3D':
+        area.spaces[0].region_3d.view_perspective = 'CAMERA'
+bpy.context.scene.camera = camera_object
+
+# print('worker connect', ('localhost', 6001 + process_num))
+IPC_START_recv = Listener(('localhost', 6001 + process_num))
+
+# IPC READY send to coordinator
+IPC_READY_sender.send(process_num)
+
+# IPC START waiting from coordinator
+msg = IPC_START_recv.accept()
+
+# play 
+time_start = time()
+
+for i in range(1, scene_frame_end + 1):
+    bpy.context.scene.frame_set(i)
+time_end = time()
+
+# IPC RESULT send to coordinator
+IPC_RESULT_sender  = Client(('localhost', 10000))
+IPC_RESULT_sender.send((time_start, time_end))
+
+bpy.ops.wm.quit_blender()
