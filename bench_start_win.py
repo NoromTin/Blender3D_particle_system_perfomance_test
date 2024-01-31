70a6b072546522ea76c5e0ec0cf7841028f44aa4
user <user@user.com>
2024-01-31 23:36:30 +0300

    results csv

--- bench_start_win.py
+++ bench_start_win.py
@@ -11,6 +11,18 @@ IPC_addr_worker_result  = ('localhost', IPC_base_port + 1)
 if  __name__ != '__main__':
     import os
     import subprocess
+    
+    # worker config
+    
+    ### windows
+    # blender_path = '%USERPROFILE%\AppData\Roaming\Blender Foundation\Blender\4.0\'
+    
+    ### mac
+    # blender_path = './Blender.app/Contents/MacOS/Blender'
+    
+    ### linux
+    # blender_path = '/usr/share/blender/4.0/'
+    
     blender_path = 'C:\\Program Files\\Blender Foundation\\Blender 3.5\\blender'
 
 if __name__ == '__main__':
@@ -19,22 +31,42 @@ if __name__ == '__main__':
     from psutil import cpu_count
     import os
     import sys
+    from sys import platform
     
     from time import sleep
     from multiprocessing.pool import Pool
+    from uuid import uuid4
+    import csv
+    
     from statistics import median
     from cpuinfo import get_cpu_info
     
     cpu_name = get_cpu_info()['brand_raw']
     
+    
+    if platform == "linux" or platform == "linux2":
+        os_type = 'lnx'
+    elif platform == "darwin":
+        os_type = 'mac'
+    elif platform == "win32":
+        os_type = 'win'
+
+    
     ############################
     ### config start###
+    
+    ### mp config
+    # multiprocessing scalibility test
+    # there 2 mp_type: 'mp' and 'th'
+    # 'mp' - multiprocessing, separate app instance for every cpu core with one core for each
+    # 'th' - multithreading, one app instance with configured core num 
     
     # number of worker - only for 'mp' test
     mp_min      = 1
     mp_max = 'auto' # 'auto' - Automatic os detect, incl hyper-threading
-    # mp_max      = 1    
-    mp_factor   = 1    # multiplier for overcore bench, for example 2 mean 24 threads for 12 logical cores
+    
+    # multiplier for overcore bench, for example 2 mean 24 threads for 12 logical cores. Experimental, mostly for incorrect logical cpu num detection 
+    mp_factor   = 1    
     
     # threads per worker - only for 'thread' test
     tn_min      = 1         # recommended for mp test, but for 
@@ -42,25 +74,28 @@ if __name__ == '__main__':
     tn_max_limit = 8
     tn_max    = 'auto'     
     # tn_max      = 1
-    tn_factor   = 1         # multiplier for overcore bench, for example 2 mean 24 threads for 12 logical cores
+    # multiplier for overcore bench, for example 2 mean 24 threads for 12 logical cores. Experimental, mostly for incorrect logical cpu num detection 
+    tn_factor   = 1 
     
-    os_type = 'win' # i known it is dirty, need some solid refactoring
+
     
-    # bench list . Bench type process are separate
+    # mp_type used in bench
     # 'mp' - for workers(num of blender instances, every with 1 core, 
     # 'thread' - one instance of blender, but starts with different num of cpu. 
-    bench_type_list = [
+    mp_type_list = [
                 'mp',
-                'thread'
+                'th'
                 ]
-    # test type. for every - separate scene python file
-    bench_name_list = [
+                
+    # test type, each have separate scene python file
+    test_type_list = [
                 'movement', 
                 #'collision'
                 ]
 
-    out_to_console  = False
+    out_to_console  = True
     out_to_csv      = True
+    csv_file_dir   = './result/'
 
     is_gui_debug    = False # True - running with gui, false - without
 
@@ -69,15 +104,15 @@ if __name__ == '__main__':
 
 def start_worker(*args):
 
-    # os_type     = args[0]
-    bench_name  = args[1]
-    # bench_type  = args[2]
-    t_num       = args[3]
-    p_num       = args[4]
-    gui_arg     = args[5]
+    ### start blender separate instance, without GUI(default), with scene py file. Exit when instance exit
+
+    test_type  = args[0]
+    t_num       = args[1]
+    p_num       = args[2]
+    gui_arg     = args[3]
     
     # name of start script here
-    blender_args = gui_arg + ' -t ' + str(t_num) + ' -P "./scene_' + bench_name + '.py"' + ' -- -pn ' + str(p_num) 
+    blender_args = gui_arg + ' -t ' + str(t_num) + ' -P "./scene_' + test_type + '.py"' + ' -- -pn ' + str(p_num) 
     cmd = '\"\"' + blender_path +'\" ' + blender_args +'\"'
     # print(cmd)
     os.system(cmd)
@@ -104,7 +139,8 @@ if __name__ == '__main__':
     
     pool = Pool(processes=mp_max)
     
-    bench_num = len(bench_name_list) *  ( (mp_max - mp_min + 1 if 'mp' in bench_type_list else 1) + (tn_max - tn_min + 1 if 'thread' in bench_type_list else 1) )
+    # progress in console
+    bench_num = len(test_type_list) *  ( (mp_max - mp_min + 1 if 'mp' in mp_type_list else 1) + (tn_max - tn_min + 1 if 'th' in mp_type_list else 1) )
     
     def start_pool(args_list):
     
@@ -138,29 +174,29 @@ if __name__ == '__main__':
     ### start bench
     result = []
     
-    for bench_name in bench_name_list:
-        for bench_type in bench_type_list:
+    for test_type in test_type_list:
+        for mp_type in mp_type_list:
      
-            if bench_type == 'mp':
+            if mp_type == 'mp':
                 for mp_num in range(mp_min, mp_max + 1):
-                    args_list = [ (os_type, bench_name,bench_type, 1 ,mp_n, gui_arg) for mp_n in range(mp_min, mp_num + 1)]
-                    result.append( (args_list[-1], tuple(start_pool(args_list) )) )
+                    args_list = [ (test_type, 1 ,mp_n, gui_arg) for mp_n in range(mp_min, mp_num + 1)]
+                    result.append( ((mp_type,) + args_list[-1], tuple(start_pool(args_list) )) )
                     i_bench +=1
                     
-            elif bench_type == 'thread':
+            elif mp_type == 'th':
                 for tn_num in range(tn_min, tn_max + 1):
-                    args_list = [ (os_type, bench_name, bench_type, tn_num , 1, gui_arg) ]
-                    result.append( (args_list[-1], tuple(start_pool(args_list) )) )
+                    args_list = [ (test_type, tn_num , 1, gui_arg) ]
+                    result.append( ((mp_type,) + args_list[-1], tuple(start_pool(args_list) )) )
                     i_bench +=1
 
     # result RAW
-    print('result raw:')
-    for rec in result:
-        print(rec)
+    if out_to_console:
+        print('result raw:')
+        for rec in result:
+            print(rec)
 
-    ### analisys
-    print('')
-    print('result analisys')
+    # analisys
+    result_analisys = []
     for rec in result:
         # average
         agg_avg = 0.0
@@ -177,7 +213,26 @@ if __name__ == '__main__':
         agg_avg /= len(rec[1])
         agg_med = median(mp_time_list)
         cpu_rating = sum([ 1/mp_time for mp_time in mp_time_list])
-        core_num = max(rec[0][3],rec[0][4])
+        core_num = max(rec[0][2],rec[0][3])
         core_rating = cpu_rating / core_num
         
-        print(f'cpu: {cpu_name} os: {rec[0][0]:4} test: {rec[0][1]:9} mp_type: {rec[0][2]:7} core_num:{core_num} cpu_rating: {cpu_rating:.6} core_rating: {core_rating:.6} avg_time: {agg_avg:.6} med_time: {agg_med:.6} min_time: {agg_min:.6} max_time: {agg_max:.6}')
+        result_analisys.append((rec[0][0],rec[0][1],core_num,cpu_rating,core_rating,agg_avg,agg_med,agg_min,agg_max))
+    
+    if out_to_console:
+        print('')
+        print('result analisys')
+        print(f'cpu : {cpu_name}  os : {os_type:4}')
+        for test in result_analisys:
+            print(f'test_type : {test[1]:9}  mp_type : {test[0]:3}  core_num : {test[2]}  cpu_rating : {test[3]:.4f}  core_rating : {test[4]:.4f}  avg_time : {test[5]:.4f}  med_time : {test[6]:.4f}  min_time : {test[7]:.4f}  max_time : {test[8]:.4f}')
+            
+    if out_to_csv:
+        # additional unique mark for result files
+        bench_hash = uuid4().hex[0:8]
+        csv_file_name = cpu_name.replace(" ", "_")+ '__' + bench_hash + '.csv'
+        with open(csv_file_dir + csv_file_name, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile #, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL
+                                    )
+            # headers
+            csv_writer.writerow(['cpu','os','test_type','mp_type','core_num','cpu_rating','core_rating','avg_time','med_time','min_time','max_time'] )
+            for test in result_analisys:
+                csv_writer.writerow([cpu_name, os_type, test[1][0:4],test[0],test[2],test[3],test[4],test[5],test[6],test[7],test[8]])
